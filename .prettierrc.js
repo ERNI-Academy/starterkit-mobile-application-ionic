fea62b074a18ac0b78e6f6f0aa34ce4c30e5bc45
Axel Taylor <axel@taylor.cat>
2022-04-14 13:37:09 +0200

    Add prettier and fix files

---
+++ .prettierrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true, // default: false
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  overrides: [
+    {
+      files: ['*.java'],
+      options: {
+        printWidth: 140,
+        tabWidth: 4,
+        useTabs: false,
+        trailingComma: 'none',
+      },
+    },
+  ],
+};
